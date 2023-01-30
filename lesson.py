3530631ceb2acf0712741c9c3e74bc08b9d5583c
tomonari <97956862+tomonari0502@users.noreply.github.com>
2023-01-30 22:07:00 +0900

    numpyの学習

--- lesson.py
+++ lesson.py
@@ -1,5 +1,33 @@
 import numpy as np
 x = np.array([1,2,3])
-print(x)
+y = np.array([2,3.9,6.1])
 
-print('テスト')
+## データの中心化
+# 平均の算出
+x.mean()
+y.mean()
+
+xc = x - x.mean()
+yc = y - y.mean()
+
+print(xc)
+print(yc)
+
+## パラメータaの計算
+# 要素ごとの掛け算（要素積）
+xx = xc * xc
+
+print(xx)
+
+xy = xc * yc
+print(xy)
+
+xxs = xx.sum()
+xys = xy.sum()
+
+print(xxs,xys)
+
+a = xy.sum()/xx.sum()
+print(a)
+
+
